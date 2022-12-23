3c119550c7109ed0a05f46722ff1970a4d1a7d5f
shkarandaroman <roman.shkaranda@ilogos.biz>
2022-12-23 18:45:52 +0200

    Edits for teamcity

--- api_collection/data/user_data.py
+++ api_collection/data/user_data.py
@@ -5,12 +5,12 @@ import string
 
 class User:
     def __init__(self, **kwargs):
-        self.id = 3404 if 'id' not in kwargs.keys() else kwargs['id']
-        self.name = 'Birjesh Ahluwalia' if 'name' not in kwargs.keys() else kwargs['name']
+        self.id = 4934 if 'id' not in kwargs.keys() else kwargs['id']
+        self.name = 'Deeptimoy Malik' if 'name' not in kwargs.keys() else kwargs['name']
         email_pref = ''.join(random.choice(string.ascii_letters) for _ in range(7))
-        self.email = 'ahluwalia_birjesh@graham.com' if 'email' not in kwargs.keys() else f'{email_pref}@mail.com' if 'email' not in kwargs.keys() else kwargs['email']
+        self.email = 'malik_deeptimoy@muller-abbott.info' if 'email' not in kwargs.keys() else f'{email_pref}@mail.com' if 'email' not in kwargs.keys() else kwargs['email']
         self.gender = 'male' if 'gender' not in kwargs.keys() else kwargs['gender']
-        self.status = 'active' if 'status' not in kwargs.keys() else kwargs['status']
+        self.status = 'inactive' if 'status' not in kwargs.keys() else kwargs['status']
 
     def to_json(self):
         return json.dumps(self.__dict__)
