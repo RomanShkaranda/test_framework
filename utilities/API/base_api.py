990093b83b32894e030b08146846c42ddac81763
shkarandaroman <roman.shkaranda@ilogos.biz>
2022-12-25 18:53:25 +0200

    Refactor data_store

--- utilities/API/base_api.py
+++ utilities/API/base_api.py
@@ -1,24 +1,15 @@
-import json
 import requests
-from CONSTANTS import ROOT_DIR
-from utilities.configuration import Configuration
+from utilities.read_json import data_store
 
 
 class BaseAPI:
     def __init__(self):
-        self.__base_url = self.get_config().application_info['API_url']
+        self.__get_config = data_store()
+        self.__base_url = self.__get_config.application_info['API_url']
         self.__user = 'admin'
-        self.__headers = {"Authorization": f'{self.get_config().application_info["token"]}'}
+        self.__headers = {"Authorization": f'{self.__get_config.application_info["token"]}'}
         self.__requests = requests
 
-    @staticmethod
-    def get_config():
-        with open(f'{ROOT_DIR}/configurations/configuration.json') as f:
-            data = f.read()
-            json_to_dict = json.loads(data)
-
-            config = Configuration(**json_to_dict)
-            return config
 
     def get(self, url, headers=None):
         if headers is None:
@@ -29,7 +20,7 @@ class BaseAPI:
     def post(self, url, body={}, headers=None):
         if headers is None:
             headers = self.__headers
-        token = {"Authorization": f'{self.get_config().application_info["token"]}'}
+        token = {"Authorization": f'{self.__get_config.application_info["token"]}'}
         response = self.__requests.post(f'{self.__base_url}{url}', headers=token, json=body)
         return response
 
