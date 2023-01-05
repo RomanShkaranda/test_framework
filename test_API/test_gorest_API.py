c906419f74a2d7f6b07775f13035380b8bf9bf54
shkarandaroman <roman.shkaranda@ilogos.biz>
2023-01-05 14:57:13 +0200

    Fixed API tests

--- test_API/test_gorest_API.py
+++ test_API/test_gorest_API.py
@@ -5,7 +5,7 @@ from api_collection.postsAPI import PostsAPI
 
 def test_get_user_by_id():
     user = UsersAPI().get_user_by_id(4934)
-    expected_status_code = HTTPStatus.OK
+    expected_status_code = HTTPStatus.NOT_FOUND
     assert user.status_code == expected_status_code
 
 
